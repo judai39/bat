e270495bbde8e5fedab2ef3affe705842f6f6522
judai39 <dhy2522609443@163.com>
2026-01-19 10:20:03 +0800

    upgrade

--- 3_logic/3.2 _set.bat
+++ 3_logic/3.2 _set.bat
@@ -11,16 +11,16 @@ if defined var (echo var的值不为空) else echo var的值为空
 ::字符串替换
 set str_test1=%var:b=c%
 echo %str_test1%
-::字符串截取
+::字符串截取(不需要比较负数和零的大小,这里的零只需要理解为最左边或最右边的数字就行了)
 ::1,3指的是数组形式中的第1-3的字符赋值
 set str_test2=%var:~1,3%
 echo %str_test2%
 ::0,-2指的是数组形式中的倒数第二个字符到第零个进行赋值
 set str_test3=%var:~0,-2%
 echo %str_test3%
-::-2指把倒数两位字符到最后一个赋值
+::-2指把倒数两位字符到最后一个赋值(相当于-2,0)
 set str_test4=%var:~-2%
 echo %str_test4%
-::2指把第二个字符后的所欲字符赋值
+::2指把第二个字符后的所有字符赋值
 set str_test5=%var:~2%
 echo %str_test5%
