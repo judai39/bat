5f61d6d1c9ab90f1faa5ffb442dce1fc5bf5d2a0
judai39 <dhy2522609443@163.com>
2026-01-20 10:59:15 +0800

    upgrade

--- 1_symbol/1.9 _type.bat
+++ 1_symbol/1.9 _type.bat
@@ -1,3 +1,10 @@
 @echo off
-@REM type filename  在命令行中打开文件(仅适用于txt)
+
+::type filename  在命令行中打开文件(仅适用于txt)
 type C:\Users\judai\Desktop\a\test.txt
+
+::type .txt .txt > newtxt.txt   合并文件
+type a.txt b.txt > new_file.txt
+
+::type组合findstr打印查找内容
+type test.txt | findstr /i "am"
